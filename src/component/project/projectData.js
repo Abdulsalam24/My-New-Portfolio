a7f6171104d5a302de6fe186b3c4432fccdddc65
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-10-23 20:42:25 +0100

    new project added

--- src/component/project/projectData.js
+++ src/component/project/projectData.js
@@ -1,4 +1,4 @@
-import project8 from '../../asset/img/project8.PNG'
+import project8 from '../../asset/img/project8.jpg'
 import project9 from '../../asset/img/project9.PNG'
 import project1 from '../../asset/img/project1.PNG'
 import project2 from '../../asset/img/project2.PNG'
@@ -36,33 +36,18 @@ export const ProjectData = [
         gitHub: 'https://github.com/Abdulsalam24/Github-Finder'
     },
     {
-        description: 'Product Marketing',
-        title: 'Prosalety',
-        writeUp: 'This is a web application that I created for uploading for-sale products. You can also be contacted if someone is interested in your product. The UI and functionality of this site were created using the firebase database and React.',
+        description: 'Visqorium',
+        title: 'Visqorium-Labs',
+        writeUp: 'This is a blockchain-based enterprise web application designed to power Next-Generation Content Creation. It was created by myself and a team for the Visqorium company. I joined the team, finished the responsive, and debugged the code.',
         html: 'Html',
         scss: 'Scss',
         react: 'React',
         git: 'git',
         review: 'Review',
-        img: project4,
-        link: 'https://product-marketing-six.vercel.app/',
-        gitHub: 'https://github.com/Abdulsalam24/Product-Marketing'
+        img: project8,
+        link: 'https://visqorium-mainsite-dev.netlify.app/',
+        gitHub: 'https://github.com/Visqorium-Labs/visqoriumlabs-main-site'
     },
-
-    // {
-    //     description : 'Ecommerce',
-    //     title : 'Shopping Cart',
-    //     writeUp : 'This is an E-commerce site built with react-redux.I focused more on getting the whole site to work with the some basic bootstrap for the styling',
-    //     html : 'Html',
-    //     scss : 'Scss',
-    //     react : 'React',
-    //     bootstrap : 'Bootstrap',
-    //     git : 'git',
-    //     review : 'Review',
-    //     img : project8,
-    //     link : 'https://abdecommerce.netlify.app/',
-    //     gitHub : 'https://github.com/Abdulsalam24/E-commerce-with-redux'
-    // },
     {
         description: 'Massage Page',
         title: 'Osteopathie',
@@ -76,18 +61,5 @@ export const ProjectData = [
         link: 'https://abdosteopathiy.netlify.app/',
         gitHub: 'https://github.com/Abdulsalam24/My-OSTEOPATHY-react-project'
     }
-    // {
-    //     description : 'Figma',
-    //     title : 'Figma Project',
-    //     writeUp : 'This is Figma challenge i got from a job challenge to build only the UI.I have very comfortable with build UI so this didnt take much of time',
-    //     html : 'Html',
-    //     scss : 'Scss',
-    //     react : 'React',
-    //     git : 'git',
-    //     review : 'Review',
-    //     img : project2,
-    //     link : 'https://abdfigmacreat.netlify.app/',
-    //     gitHub : 'https://github.com/Abdulsalam24/My-OSTEOPATHY-react-project'
-    // }
 
 ]
