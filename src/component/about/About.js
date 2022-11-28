ea158a622b1dc1dad38bf6ac29f99c5036ab6b64
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-11-28 10:45:32 +0100

    new change

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -12,7 +12,7 @@ const About = () => {
           <div className="text">
 
             <p>
-              I am a Frontend Developer with <span className="green">two years of experience</span> creating user interfaces with clean, reusable code using React. I create a web application with the most recent tools and have prior experience <span className="green">working in a team</span>. I am constantly striving to build and advance a project as quickly as possible. When I'm working on a project, I'm eager and passionate about making sure you're completely satisfied with the service I provide.
+              I am a Frontend Developer with <span className="green">2+ years of experience</span> creating user interfaces with clean, reusable code using React. I create a web application with the most recent tools and have prior experience <span className="green">working in a team</span>. I am constantly striving to build and advance a project as quickly as possible. When I'm working on a project, I'm eager and passionate about making sure you're completely satisfied with the service I provide.
 
             </p>
             <p>Here are some of the tools I'm currently using:</p>
